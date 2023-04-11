5f48779baa553b9547b8f06842a2c3256006c5e9
Ajay Daniel Trevor <ajaydtrevor@gmail.com>
2023-04-11 11:28:26 +0530

    added title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Head>
+        <title>AI Ultra</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
